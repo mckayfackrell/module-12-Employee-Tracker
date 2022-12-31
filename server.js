73ad37c5a3added1ba06b0fcc7f667de914feb23
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-12-31 14:32:56 -0700

    added welcome message, inital prompt and connection to database

--- server.js
+++ server.js
@@ -1,21 +1,117 @@
-const express = require('express');
-//Import and require mysql2
-const mysql = require('mysql2');
+// Import and require mysql2
+const mysql = require("mysql2");
+// Import inquirer
+const inquirer = require("inquirer");
+// Import console.table
+const cTable = require("console.table");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+require("dotenv").config();
 
-// Express middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Welcome title in terminal window when starting database
-const welcomeLogo = () => {
-  console.log(chalk.green(figlet.textSync('\n欢迎 \nWelcome to \nEmployee Tracker', { font: 'standard', horizontalLayout: 'default', width: 80 })));
-};
-welcomeLogo();
-
-// Force false so data doesn't get dropped on every sync
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// Connect to database
+const connection = mysql.createConnection({
+  host: "127.0.0.1", // This was changed from 'localhost' because of use with macOS
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "employee_db",
 });
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+afterConnection = () => {
+  console.log("***********************************");
+  console.log("*           WELCOME TO            *");
+  console.log("*        EMPLOYEE MANAGER         *");
+  console.log("*               :)                *");
+  console.log("***********************************");
+  promptUser();
+};
+
+const promptUser = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choices",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Update an employee manager",
+          "View employees by department",
+          "Delete a department",
+          "Delete a role",
+          "Delete an employee",
+          "View department budgets",
+          "No Action",
+        ],
+      },
+    ])
+    .then((answers) => {
+      const { choices } = answers;
+
+      if (choices === "View all departments") {
+        showDepartments();
+      }
+
+      if (choices === "View all roles") {
+        showRoles();
+      }
+
+      if (choices === "View all employees") {
+        showEmployees();
+      }
+
+      if (choices === "Add a department") {
+        addDepartment();
+      }
+
+      if (choices === "Add a role") {
+        addRole();
+      }
+
+      if (choices === "Add an employee") {
+        addEmployee();
+      }
+
+      if (choices === "Update an employee role") {
+        updateEmployee();
+      }
+
+      if (choices === "Update an employee manager") {
+        updateManager();
+      }
+
+      if (choices === "View employees by department") {
+        employeeDepartment();
+      }
+
+      if (choices === "Delete a department") {
+        deleteDepartment();
+      }
+
+      if (choices === "Delete a role") {
+        deleteRole();
+      }
+
+      if (choices === "Delete an employee") {
+        deleteEmployee();
+      }
+
+      if (choices === "View department budgets") {
+        viewBudget();
+      }
+
+      if (choices === "No Action") {
+        connection.end();
+      }
+    });
+};
